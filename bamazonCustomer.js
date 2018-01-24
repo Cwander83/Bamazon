9e46cdc292f304b3705c1e5f685c032389492409
Cwander83 <chriswandermail@gmail.com>
2018-01-24 14:37:19 -0500

    1-24-18

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,16 +2,12 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 // call once somewhere in the beginning of the app
 const cTable = require('console.table');
-
-
 // create the connection information for the sql database
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-
     // Your username
     user: "root",
-
     // Your password
     password: "Supra1234",
     database: "bamazon_db"
@@ -20,49 +16,12 @@ var connection = mysql.createConnection({
 // connect to the mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
-    console.log('connected to sql with id: ' + connection.threadId);
+    //console.log('connected to sql with id: ' + connection.threadId);
     // run the start function after the connection is made to prompt the user
     //where you call the first function -----
     myTable();
 
 });
-
-
-
-function start() {
-
-    console.log("Selecting all orders...\n");
-    connection.query("SELECT * FROM orders", function (err, res) {
-        if (err) throw err;
-        // Log all results of the SELECT statement
-
-        inquirer.prompt([{
-
-                    name: 'id',
-                    type: 'input',
-                    message: 'Please select the Item # of item to purchase',
-                    validate: function validateOrder(order) {
-                        var reg = /^([1-9]|10)$/;
-                        return reg.test(order) || "Please enter a valid ItemId";
-                    }
-                },
-                {
-                    name: 'quantity',
-                    type: 'input',
-                    message: 'How many would you like to purchase?',
-                    validate: function (value) {
-                        if (isNaN(value) === false) {
-                            return true;
-                        }
-                        return false;
-                    }
-                }
-            ])
-            .then((answer) => {
-                purchase(answer.id, answer.quantity);
-            });
-    });
-};
 
 function myTable() {
     connection.query('SELECT * FROM orders', (err, res) => {
@@ -81,14 +40,53 @@ function myTable() {
     start();
 };
 
-function purchase(id, quantity) {
+function start() {
+    console.log('\n  **  WELCOME TO BAMAZON  **\n');
+    console.log('HOME OF ALL YOUR TERMINAL SHOPPING NEEDS\n');
+    connection.query("SELECT * FROM orders", (err, res) => {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+
+        inquirer.prompt([{
+
+                    name: 'id',
+                    type: 'input',
+                    message: 'Please select the Item # of item to purchase',
+                    validate: function validateOrder(order) {
+                        let reg = /^([1-9]|10)$/;
+                        return reg.test(order) || "Please enter a valid ItemId";
+                    }
+                },
+                {
+                    name: 'quantitySold',
+                    type: 'input',
+                    message: 'How many would you like to purchase?',
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        }
+                        return console.log("not a proper number");;
+                    }
+                }
+            ])
+            .then((answer) => {
+                purchase(answer.id, answer.quantitySold);
+            });
+    });
+};
+
+
+
+function purchase(id, quantitySold, name) {
     let query = 'SELECT * FROM orders WHERE ?';
     connection.query(query, {
+        
         item_id: id
     }, function (err, res) {
         console.log('----------------------------');
         console.log(`id of product: ${id}`);
-        console.log(`quantity of purchase: ${quantity}`);
+        console.log(`name of product: ${res[id].name}`);
+        console.log(`quantity of purchase: ${quantitySold}`);
         console.log('----------------------------');
         console.table(myTable);
         myTable();
